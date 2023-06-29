224c349aa14b78493889bf90e5e057c850383bcc
tribc-0220 <bui.cong.tri@sun-asterisk.com>
2023-06-29 11:20:25 +0700

    Add JwtUtils

---
+++ utils/JwtUtils.php
@@ -0,0 +1,88 @@
+<?php
+
+trait JwtUtils
+{
+    private static $secretToken = "432A462D4A614E635266556A586E3272357538782F413F4428472B4B62506553";
+    private static $expirationTime = 3600 * 24;
+
+    function base64UrlEncode($data)
+    {
+        $base64 = base64_encode($data);
+        return str_replace(['+', '/', '='], ['-', '_', ''], $base64);
+    }
+
+    function base64UrlDecode($data)
+    {
+        $base64 = str_replace(['-', '_'], ['+', '/'], $data);
+        $base64Pad = $base64 . substr('==', (2 - strlen($data) * 3) % 4);
+        return base64_decode($base64Pad);
+    }
+
+    private function createJwtToken($header, $payload): string
+    {
+        $headerJson = json_encode($header);
+        $payloadJson = json_encode($payload);
+
+        $headerBase64 = $this->base64UrlEncode($headerJson);
+        $payloadBase64 = $this->base64UrlEncode($payloadJson);
+
+        $signature = hash_hmac('sha256', $headerBase64 . '.' . $payloadBase64, self::$secretToken, true);
+        $signatureBase64 = $this->base64UrlEncode($signature);
+
+        return $headerBase64 . '.' . $payloadBase64 . '.' . $signatureBase64;
+    }
+
+    function generateToken($user): string
+    {
+        $header = ['alg' => 'HS256', 'typ' => 'JWT'];
+        $payload = ['user' => ['code' => $user['code'], 'email' => $user['email']], 'iat' => time(), 'exp' => time() + self::$expirationTime];
+        return $this->createJwtToken($header, $payload, self::$secretToken);
+    }
+
+    private function verifyJwtToken($jwtToken): bool
+    {
+        $jwtParts = explode('.', $jwtToken);
+        if (count($jwtParts) !== 3) {
+            return false;
+        }
+        $headerBase64 = $jwtParts[0];
+        $payloadBase64 = $jwtParts[1];
+        $signatureBase64 = $jwtParts[2];
+        $signature = $this->base64UrlDecode($signatureBase64);
+        $expectedSignature = hash_hmac('sha256', $headerBase64 . '.' . $payloadBase64, self::$secretToken, true);
+
+        if (!hash_equals($signature, $expectedSignature)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function isTokenValid($token): bool
+    {
+        if (!$this->base64UrlDecode($token)) {
+            return false;
+        } else {
+            $splitToken = explode('.', $token);
+            $payload = $this->base64UrlDecode($splitToken[1]);
+            $header = $this->base64UrlDecode($splitToken[0]);
+            if (isset($payload->exp) && $payload->exp < time()) {
+                return false;
+            } else {
+                $isValid = $this->verifyJwtToken($token, self::$secretToken);
+                if ($isValid) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    function extractUser($token)
+    {
+        $token = explode('.', $token);
+        $payload = json_decode($this->base64UrlDecode($token[1]));
+        return $payload->user;
+    }
+}
